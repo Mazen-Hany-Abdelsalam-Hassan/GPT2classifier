ecfde962ea92fa9c84d280608f0506e2639252f2
Mazen-Hany-Abdelsalam-Hassan <mazenhany1912003@gmail.com>
2025-04-19 15:29:45 +0200

    Improve dataloader

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 from config import *
-from data_utils import *
+from data_utils import split_data, CreateDataloader
 from GPT2 import   GPTModel
 from GPT2Modification import ClassificationModel
--- src/data_utils.py
+++ src/data_utils.py
@@ -1,13 +1,61 @@
-import os
 import pandas as pd
 from config import *
+import os
 import torch
-from torch.utils.data import Dataset
+from torch.utils.data import Dataset , DataLoader
 
-def split_data(data: pd.DataFrame, train_split: int = .8, val_split: int = .10):
+def dynamic_batch_loader(batch: list, padding_token=PADDINGTOKEN):
+    max_len = max(len(x) for x, _ in batch)
+
+    X, Y = [], []
+    for x, y in batch:
+        padded_x = x + [padding_token] * (max_len - len(x))
+        X.append(padded_x)
+        Y.append(y)
+
+    return torch.tensor(X, dtype=torch.long), torch.tensor(Y)
+
+
+def sort_df(data:pd.DataFrame,tokenizer = TOKENIZER):
+    data.loc[::,'encoded_text'] = data.loc[::,'review'].apply(tokenizer.encode)
+    data.loc[::,'length'] = data.loc[::,'encoded_text'].apply(len)
+    sorted_df = data.sort_values("length")
+    return sorted_df.reset_index( drop=True)
+
+
+
+class ClassifierDataset(Dataset):
+    def __init__(self, csv_dir, allowed_seq_length = ALLOWED_SEQ_LENGTH,tokenizer = TOKENIZER):
+        super().__init__()
+        self.data = pd.read_csv(csv_dir)
+        self.data = sort_df(self.data , tokenizer = tokenizer)
+        self.X = self.data.loc[::, 'encoded_text']
+        self.Y = self.data.loc[::, 'sentiment']
+        self.allowed_seq_length = allowed_seq_length
+    def __getitem__(self, idx):
+        x = self.X[idx]
+        x = x[:self.allowed_seq_length-1]
+        x.append(PADDINGTOKEN)
+        y = self.Y[idx]
+        return x , y
+    def __len__(self):
+        return len(self.X)
+
+def CreateDataloader(csv_dir, batch_size = 16,
+                      allowed_seq_length = ALLOWED_SEQ_LENGTH,
+                      tokenizer = TOKENIZER,
+                      collate_fn=dynamic_batch_loader):
+    dataset = ClassifierDataset(csv_dir=csv_dir , allowed_seq_length=allowed_seq_length,
+                                tokenizer=tokenizer)
+    dataloader = DataLoader(dataset , batch_size=batch_size , collate_fn=collate_fn)
+    return  dataloader
+
+
+
+def split_data(data: pd.DataFrame, train_split: int = .8, val_split: int = .10 , seed = SEED):
     os.makedirs(PARENT_DIR, exist_ok=True)
     os.makedirs(DATA_PATH, exist_ok=True)
-    data = data.sample(frac=1, random_state=SEED).reset_index(drop=True)
+    data = data.sample(frac=1, random_state=seed).reset_index(drop=True)
     data_size = len(data)
     train_size = int(train_split * data_size)
     val_size = int((train_split + val_split) * data_size)
@@ -20,27 +68,3 @@ def split_data(data: pd.DataFrame, train_split: int = .8, val_split: int = .10):
     train_df.to_csv(train_df_dir, index=False)
     val_df.to_csv(val_df_dir, index=False)
     test_df.to_csv(test_df_dir, index=False)
-
-
-class ClassifierDataset(Dataset):
-    def __init__(self, csv_dir, tokenizer):
-        super().__init__()
-        data = pd.read_csv(csv_dir)
-        self.X = data.loc[::, 'review'].apply(tokenizer.encode)
-        self.Y = data.loc[::, 'sentiment']
-
-    def __getitem__(self, idx):
-        x = self.X[idx]
-        y = self.Y[idx]
-        # print(x)
-        length = len(x)
-        diff = ALLOWED_SEQ_LENGTH - length
-        if diff >= 0:
-            padding = diff * [PADDINGTOKEN]
-            x = x + padding
-        else:
-            x = x[:ALLOWED_SEQ_LENGTH]
-        return torch.tensor(x), torch.tensor(y)
-
-    def __len__(self):
-        return len(self.X)
