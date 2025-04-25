1828ff1217c5b93848cdea293d2a2ad22526298b
Mazen-Hany-Abdelsalam-Hassan <mazenhany1912003@gmail.com>
2025-04-25 23:54:24 +0300

    Adding Low Rank adaptation

--- src/__init__.py
+++ src/__init__.py
@@ -1,7 +1,7 @@
 from data_utils import split_data, CreateDataloader
 from GPT2 import   GPTModel
 from config import *
-from GPT2Modification import ClassificationModel
+from GPT2Modification import ClassificationModel, LoRA_Classification_Model
 from train_evaluate import train_classifier , evaluate_classifier
 from create_config_file import create_json
 from lorautils import ReplaceLinear
