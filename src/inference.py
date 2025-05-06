81f7064be65c8240f4093a27f65887d32d07bc22
Mazen <mazenhany1912003@gmail.com>
2025-05-06 16:23:13 +0300

    without mixed and parallelization

---
+++ src/inference.py
@@ -0,0 +1,15 @@
+from src import TOKENIZER  , PADDINGTOKEN
+from torch import  tensor
+from config import DEVICE ,LABEL_DICTIONARY_IMDB ,ALLOWED_SEQ_LENGTH
+def predict(text: 'str', model, tokenizer = TOKENIZER,
+            padding_token:int= PADDINGTOKEN ,label_dict:dict= LABEL_DICTIONARY_IMDB ,
+            Max_length:int = ALLOWED_SEQ_LENGTH):
+    assert len(text) > 0 , "Empty String"
+    encoded_text = tokenizer.encode(text)[:Max_length]
+    encoded_text.append(padding_token)
+    encoded_text = tensor([encoded_text])
+    model.to(DEVICE)
+    result = model(encoded_text)[:,-1,0].argmax(axis=-1 )
+    inverse_label_dict = {value : key  for key , value in LABEL_DICTIONARY_IMDB.items()}
+    return  inverse_label_dict[result]
+
